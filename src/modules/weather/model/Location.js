2a71aceb2dc7910de4baeae584496989faee0f12
Huon Imberger <huon@plonq.org>
2018-05-18 14:51:04 +1000

    Implement Location with hardcoded location for testing

--- src/modules/weather/model/Location.js
+++ src/modules/weather/model/Location.js
@@ -1,5 +1,21 @@
+// @flow
 import { LocationInterface } from './LocationInterface';
 
 export default class Location implements LocationInterface {
     // please complete the implementation of the concrete class based on the interface it implements
+    _latitude;
+    _longitude;
+
+    constructor() {
+        this._latitude = '-37.9';
+        this._longitude = '144.9';
+    }
+
+    getLatitude(): ?string {
+        return String(this._latitude);
+    }
+
+    getLongitude(): ?string {
+        return String(this._longitude);
+    }
 }
